504ddb5c5d079c84a8d7ea147742e7dcc2c58c43
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-08-06 02:00:47 -0500

    Fix concmd to only actually attempt to change if it has a correct argument

--- Server Addon/gspeak2/lua/gspeak/cl_concmd.lua
+++ Server Addon/gspeak2/lua/gspeak/cl_concmd.lua
@@ -3,6 +3,7 @@
 concommand.Add("gs_broadcast", function(ply, cmd, args) -- main implementation
   if #args ~= 1 or (args[1] ~= "1" and args[2] ~= "0") then
     print("Incorrect arguments. Give 1 to turn on broadcast, 0 to turn it off.")
+    return
   end
 
   local value = args[1] == "1"
