853d68349d84e4d73dc3624d4b2767e00a0702da
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-08-06 04:21:46 -0500

    Add +/-gs_broadcast commands

--- Server Addon/gspeak2/lua/gspeak/cl_concmd.lua
+++ Server Addon/gspeak2/lua/gspeak/cl_concmd.lua
@@ -1,13 +1,5 @@
 
-
-concommand.Add("gs_broadcast", function(ply, cmd, args) -- main implementation
-  if #args ~= 1 or (args[1] ~= "1" and args[1] ~= "0") then
-    print("Incorrect arguments. Give 1 to turn on broadcast, 0 to turn it off.")
-    return
-  end
-
-  local value = args[1] == "1"
-
+local function RequestBroadcast(ply, state)
   if ply:IsAdmin() then
     print("Requesting broadcast state change")
     net.Start("gspeak_broadcast")
@@ -16,4 +8,20 @@ concommand.Add("gs_broadcast", function(ply, cmd, args) -- main implementation
   else
     print("Not an admin!")
   end
+end
+
+concommand.Add("gs_broadcast", function(ply, cmd, args) -- main implementation
+  if #args ~= 1 or (args[1] ~= "1" and args[1] ~= "0") then
+    print("Incorrect arguments. Give 1 to turn on broadcast, 0 to turn it off.")
+    return
+  end
+
+  RequestBroadcast(ply, args[1] == "1")
 end, nil, "Allows server admins to broadcast to all GSpeak users.")
+
+concommand.Add("+gs_broadcast", function(ply)
+  RequestBroadcast(ply, true)
+end, nil, "Enables GSpeak broadcasting")
+concommand.Add("-gs_broadcast", function(ply)
+  RequestBroadcast(ply, true)
+end, nil, "Disables GSpeak broadcasting")
